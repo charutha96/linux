fabccf4fdcedca2c26fea58c4e4345e9b5643601
charutha96 <charuthapc966@gmail.com>
2019-03-19 20:35:52 +0530

    first commit

---
+++ arithmetic.sh
@@ -0,0 +1,8 @@
+echo "enter two numbers"
+read a b
+echo "$a+$B="`expr $a + $ b`
+echo "$a+$B="`expr $a - $ b`
+echo "$a+$B="`expr $a \* $ b`
+echo "$a+$B="`expr $a / $ b`
+echo "$a+$B="`expr $a % $ b`
+
