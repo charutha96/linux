939697671fc915466788e95e9fecb40e7ecf7116
charutha96 <charuthapc966@gmail.com>
2019-03-26 19:37:52 +0530

    commit

--- greatest.sh
+++ greatest.sh
@@ -1,9 +1,10 @@
-echo"enter first number"
+echo " enter first number "
 read a
-echo"enter second number"
+echo " enter second number "
 read b
-if[ $a - gt $b]
+if [ $a -gt $b ]
 then 
-echo"greatesh number is"$a
-echo"greatesh number is"$b
+echo " greatest number is " $a
+else
+echo " greatest number is " $b
 fi
