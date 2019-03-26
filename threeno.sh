c756da66a03466e4595f02c5d82314350e403375
charutha96 <charuthapc966@gmail.com>
2019-03-26 20:16:20 +0530

    commit

---
+++ threeno.sh
@@ -0,0 +1,9 @@
+echo " enter three numbers: "
+read a b c
+sum=` expr $a + $b + $c `
+avg=`expr $sum / 3`
+pro=`expr $a \* $b \* $c`
+echo "sum= " $sum
+echo "average= " $avg
+echo "product= " $pro
+
