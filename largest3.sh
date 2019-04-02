1522e87480d933f64b44d5033d711a1799b017d4
charutha96 <charuthapc966@gmail.com>
2019-04-02 19:25:54 +0530

    commit

---
+++ largest3.sh
@@ -0,0 +1,19 @@
+echo "Enter three numbers:"
+read a b c
+if [ $a -gt $c ]
+then
+if [ $a -gt $c ]
+then 
+echo $a "is greater"
+else
+echo $c "is greater"
+fi
+else
+if  [$b -gt $c]
+then
+echo $b "is greater"
+else
+echo $C "is greater"
+fi
+fi
+
